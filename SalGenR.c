9e7d0c0c549a6c24111a54b7e9314f255d03e7c8
Jibble Einux <50579963+JibbleEinuxCODE@users.noreply.github.com>
2020-10-22 10:31:40 +0600

    Modifiy more

--- SalGenR.c
+++ SalGenR.c
@@ -1,13 +1,20 @@
+//this project name is SalGenR
+//This project for Employees details store in companies Database..
+// Using this Project we can handle our Employees Details
+// We can add new employee , we can search Employee, We can Modify Employees Records
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define _Username_ "Admin"
-#define _Password_ "Admin"
+#define _Username_ "Admin" //Define a Default Username for Login
+#define _Password_ "Admin" //Define a Default Password for Login
 
+//Declaring a Structure for store employees Details
 struct employee
 {
+    //properties of a Employee
     char name[20];
     int age;
     int phone_number;
@@ -16,19 +23,18 @@ struct employee
 } * head, *tail;
 
 // login section
-
 bool loginCheck(char name[15], char pass[15])
 {
-    if (strcmp(name, _Username_) == 0)
+    if (strcmp(name, _Username_) == 0) //checking input Username to stored Username
     {
-        if (strcmp(pass, _Password_) == 0)
+        if (strcmp(pass, _Password_) == 0) //checking input password to stroed Password
         {
-            return true;
+            return true; //if Username and Password are matched then it returns a truth value
         }
     }
     else
     {
-        return false;
+        return false; //if Username and Password are not matched then it returns a flase value
     }
 }
 
@@ -160,6 +166,36 @@ void print_employee_list()
     }
 }
 
+void search_Employee_By_Name()
+{
+    char value[20];
+    struct employee *searchNode = head;
+    int flag = 0;
+    getchar();
+    gets(value);
+    while (searchNode != NULL)
+    {
+        if (strlwr(searchNode->name) == strlwr(value))
+        {
+            printf("\n\nSearch Result.\n");
+            printf("-----------------\n");
+            printf("%s Name employee present in this database.\n", value);
+            printf("Name: ");
+            puts(searchNode->name);
+            printf("Age: %d\n", searchNode->age);
+            printf("Phone Number: 0%d\n", searchNode->phone_number);
+            printf("Salary: %.2lf\n", searchNode->salary);
+            flag = 1;
+            searchNode = searchNode->next;
+        }
+        else
+            searchNode = searchNode->next;
+    }
+
+    if (flag == 0)
+        printf("Employee not found\n");
+}
+
 void search_Employee_By_Age()
 {
     int value;
@@ -170,9 +206,104 @@ void search_Employee_By_Age()
     {
         if (searchNode->age == value)
         {
-            printf("%d is present in this list. Memory address is %d\n", value, searchNode);
+            printf("\n\nSearch Result.\n");
+            printf("-----------------\n");
+            printf("%d Age employee present in this database.\n", value);
+            printf("Name: ");
+            puts(searchNode->name);
+            printf("Age: %d\n", searchNode->age);
+            printf("Phone Number: 0%d\n", searchNode->phone_number);
+            printf("Salary: %.2lf\n", searchNode->salary);
             flag = 1;
-            break;
+            searchNode = searchNode->next;
+        }
+        else
+            searchNode = searchNode->next;
+    }
+
+    if (flag == 0)
+        printf("Employee not found\n");
+}
+
+void search_Employee_By_phone()
+{
+    int value;
+    struct employee *searchNode = head;
+    int flag = 0;
+    scanf("%d", &value);
+    while (searchNode != NULL)
+    {
+        if (searchNode->phone_number == value)
+        {
+            printf("\n\nSearch Result.\n");
+            printf("-----------------\n");
+            printf("%d Phone Number employee present in this database.\n", value);
+            printf("Name: ");
+            puts(searchNode->name);
+            printf("Age: %d\n", searchNode->age);
+            printf("Phone Number: 0%d\n", searchNode->phone_number);
+            printf("Salary: %.2lf\n", searchNode->salary);
+            flag = 1;
+            searchNode = searchNode->next;
+        }
+        else
+            searchNode = searchNode->next;
+    }
+
+    if (flag == 0)
+        printf("Employee not found\n");
+}
+
+void search_Employee_By_Salary()
+{
+    double value;
+    struct employee *searchNode = head;
+    int flag = 0;
+    scanf("%d", &value);
+    while (searchNode != NULL)
+    {
+        if (searchNode->salary == value)
+        {
+            printf("\n\nSearch Result.\n");
+            printf("-----------------\n");
+            printf("%ld Salary employee present in this database.\n", value);
+            printf("Name: ");
+            puts(searchNode->name);
+            printf("Age: %d\n", searchNode->age);
+            printf("Phone Number: 0%d\n", searchNode->phone_number);
+            printf("Salary: %.2lf\n", searchNode->salary);
+            flag = 1;
+            searchNode = searchNode->next;
+        }
+        else
+            searchNode = searchNode->next;
+    }
+
+    if (flag == 0)
+        printf("Employee not found\n");
+}
+
+void search_Employee_By_Place()
+{
+    char value[20];
+    struct employee *searchNode = head;
+    int flag = 0;
+    getchar();
+    gets(value);
+    while (searchNode != NULL)
+    {
+        if (strlwr(searchNode->name) == strlwr(value))
+        {
+            printf("\n\nSearch Result.\n");
+            printf("-----------------\n");
+            printf("%s area employee present in this database.\n", value);
+            printf("Name: ");
+            puts(searchNode->name);
+            printf("Age: %d\n", searchNode->age);
+            printf("Phone Number: 0%d\n", searchNode->phone_number);
+            printf("Salary: %.2lf\n", searchNode->salary);
+            flag = 1;
+            searchNode = searchNode->next;
         }
         else
             searchNode = searchNode->next;
@@ -191,7 +322,7 @@ int main()
 {
     head = NULL;
     tail = NULL;
-    int option, option1;
+    int option, option1, option2;
     char name[15], pass[15], name1[20];
     double salary;
     printLogo();
@@ -245,7 +376,38 @@ int main()
                 case 4:
                     break;
                 case 5:
-                    search_Employee_By_Age();
+                    //case 5 ----Search section start
+                    printf("\nChoose your Searching Option.\n");
+                    printf("-------------------------------\n");
+                    printf("1.Search by Name\n");
+                    printf("2.Search by Age\n");
+                    printf("3.Search by Phone Number\n");
+                    printf("4.Search by Salary\n");
+                    printf("5.Search by Place\n");
+                    printf("Search>> ");
+                    scanf("%d", option2);
+                    switch (option2)
+                    {
+                    case 1:
+                        search_Employee_By_Name();
+                        break;
+                    case 2:
+                        search_Employee_By_Age();
+                        break;
+                    case 3:
+                        search_Employee_By_phone();
+                        break;
+                    case 4:
+                        search_Employee_By_Salary();
+                        break;
+                    case 5:
+                        search_Employee_By_Place();
+                        break;
+                    default:
+                        printf("You have to choose right option\n");
+                        break;
+                    }
+                    //case 5 ----search section End
                     break;
                 case 6:
                     break;
